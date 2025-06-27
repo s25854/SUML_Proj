709693734aa3542068da5f0a0e5b536e93813ac5
s25854 <sokkuba66@gmail.com>
2025-06-27 11:07:55 +0200

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -28,7 +28,10 @@ def get_user_id():
 
 @st.cache_resource
 def get_mongo_client():
-    uri = st.secrets["MONGO_URI"]
+    try:
+        uri = st.secrets["MONGO_URI"]
+    except Exception:
+        uri = os.environ.get("MONGO_URI")
     if not uri:
         st.error("Nie znaleziono URI do MongoDB.")
         st.stop()
